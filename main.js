402f975c4cc447c1432dee97cfe821997a49cbbc
Miroslav Kotalik <mikotalik@seznam.cz>
2024-11-10 18:40:15 +0100

    Update font, sizing, mobile version

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ onmousemove = (e) => {
     const x = e.pageX
     const y = e.pageY
 
-    preview.setAttribute("camera-orbit", (24 + x*-0.01) + "deg" + (106 + y*-0.01) + "deg" + "490m")
+    let scale = 2560/innerWidth
+
+    preview.setAttribute("camera-orbit", (24 + x*-0.01) + "deg" + (112 + y*-0.01) + "deg" + (450*scale) + "m")
 }
